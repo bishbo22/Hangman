0a1848bfdc96af801ad34ce2fcfcbd7911ae7b29
blair <bishbo22@wfu.edu>
2024-04-30 17:14:38 -0400

    Almost Final Version

--- src/Hangman.java
+++ src/Hangman.java
@@ -1,42 +1,69 @@
 import java.awt.*;
 import java.util.Arrays;
 import java.util.Scanner;
-
-import static java.util.Collections.replaceAll;
-
+/**********************************************************************
+ * @file Hangman.java
+ * @brief runs an interactive hangman game with the console and drawing panel
+ * @author Blair Bishop, Morgan White, Jack Connolly
+ * @date: 4/30/24
+ ***********************************************************************/
 public class Hangman {
 
     public static void main(String[] args) {
+        //Create objects to make a scanner and randomize a country from the Word class
         Scanner scan = new Scanner(System.in);
         String country = Word.randomCountry().toLowerCase();
+
+        //Create a character array to make answer as long as the letters in the country's name
         char[] answer = new char[country.length()];
+
+        //Create a drawing panel to be drawn on and initialize g as a graphics variable
         DrawingPanel panel = new DrawingPanel(500, 500);
         Graphics g = panel.getGraphics();
+
+        //This fills answer with underscores so the user cannot see what the letters are, but can see how many there are
         Arrays.fill(answer, '_');
+
         char input;
         boolean stillPlaying = true;
         int mistakeCounter = 0;
         int totalSimilarities = 0;
+
+        //Create answer1 and answer2 to later help separate the previous guess from the new guess when analyzing if the user made a mistake or not
         char[] answer1 = new char[country.length()];
         char[] answer2;
+
+        //Draw the initial hangman board and show the empty word (answer) as a string
         drawHangman(g,answer,mistakeCounter,country);
+
+        //Allow the player to play until they win or lose based on the value of stillPlaying
         while (stillPlaying) {
+            //Copies the previous value of answer to answer 1
             System.arraycopy(answer, 0, answer1, 0, answer.length);
+
+            //Asks for and stores the user's response to the prompt
             System.out.print("Guess a letter: ");
             input = scan.nextLine().charAt(0);
+
+            //Checks if the user's letter is in the country's name, but then changes the empty space in answer to the letter the user correctly guessed
             answer2 = Word.match(input,country,answer);
+
+            //Checks if the previous answer1 and new answer2 match or not to see if the user successfully guessed the letter, failure means the total similarities will be the length of the country's name
             for (int i = 0; i < answer.length; i++) {
                 if (answer1[i] == answer2[i]) {
                     totalSimilarities++;
                 }
             }
+
+            //If the similarities between answer1 and answer2 are the length of the country's name, then that means the user made a mistake and counts that under mistakeCounter
             if (totalSimilarities == answer.length) {
                 mistakeCounter++;
             }
-            if (mistakeCounter == 6) {
-                stillPlaying = false;
-            }
+
+            //Sets the totalSimilarities to 0 so that the while loop can run again with answer1 and answer2 able to be compared without the previous round's similarities being reflected
             totalSimilarities = 0;
+
+            //Draws the hangman setup again and this time puts in the correct letter or the body part depending on the output of drawHangman (true or false)
             if (drawHangman(g,answer2,mistakeCounter,country)) {
                 stillPlaying = false;
             }
@@ -44,6 +71,17 @@ public class Hangman {
         System.out.println(country);
     }
 
+    /**
+     * drawHangman() takes in these parameters and draws the hangman setup in the drawing panel using Graphics. Then, it
+     * tests how many mistakes that the user has made to determine how many body parts to draw. Then, it will show the
+     * user if they won or not depending on whether they guessed the word correctly or made 6 mistakes. It then returns over,
+     * a variable that is true when an end-of-game decision is made.
+     * @param g : Graphics,
+     * @param answer : char[],
+     * @param mistakeCounter : int,
+     * @param country : String,
+     * @return boolean
+     */
     public static boolean drawHangman(Graphics g,char[] answer,int mistakeCounter,String country) {
         boolean over = false;
         g.setColor(Color.white);
